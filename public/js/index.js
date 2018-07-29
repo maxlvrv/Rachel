b2072dc51609391e6251f46c6c06ffc9999a5813
Joan <joan@example.com>
2018-07-29 22:00:16 +0100

    add loop audio

--- public/js/index.js
+++ public/js/index.js
@@ -19,5 +19,9 @@ function onClick() {
 function play(encodedMP3) {
   // Play the audio on the server
   var audio = new Audio('/birthday.mp3');
+	audio.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+}, false);
   audio.play();
 }
